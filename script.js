35bc9cbcda154f5d89873f790023c5f84ea94ee0
suzannegivnish@gmail.com <suzannegivnish@gmail.com>
2020-10-16 14:44:30 -0400

    updated portfolio page

---
+++ script.js
@@ -0,0 +1,31 @@
+
+$('#travelBtn').on('click', function () {
+    console.log('Travel');
+    window.location.href = 'https://dandandanoneil.github.io/travel-companion/index.html';
+});
+
+$('#travelGitBtn').on('click', function () {
+    console.log('Travel Github');
+    window.location.href = 'https://github.com/dandandanoneil/travel-companion';
+});
+
+$('#weatherBtn').on('click', function () {
+    console.log('Weather');
+    window.location.href = 'https://suzygiv.github.io/Weather-Dashboard/';
+});
+
+$('#weatherGitBtn').on('click', function () {
+    console.log('Weather Github');
+    window.location.href = 'https://github.com/suzygiv/Weather-Dashboard';
+});
+
+$('#codeBtn').on('click', function () {
+    console.log('Code');
+    window.location.href = 'https://suzygiv.github.io/Code-Quiz/';
+});
+
+$('#codeGitBtn').on('click', function () {
+    console.log('Code Github');
+    window.location.href = 'https://github.com/suzygiv/Code-Quiz';
+});
+
