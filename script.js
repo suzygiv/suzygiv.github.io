771403d44358eb1dd8be2dc75a82f77e272a523a
suzygiv <suzannegivnish@gmail.com>
2021-02-04 18:58:09 -0500

    updated js

--- script.js
+++ script.js
@@ -1,177 +1,177 @@
 //Travel Companion Buttons
 $('#travelBtn').on('click', function () {
     console.log('Travel');
-    window.location.href = 'https://dandandanoneil.github.io/travel-companion/index.html';
+    window.open('https://dandandanoneil.github.io/travel-companion/index.html', "_blank");
 });
 
 $('#travelGitBtn').on('click', function () {
     console.log('Travel Github');
-    window.location.href = 'https://github.com/dandandanoneil/travel-companion';
+    window.open('https://github.com/dandandanoneil/travel-companion', "_blank");
 });
 
 //Weather Dashboard Buttons
 $('#weatherBtn').on('click', function () {
     console.log('Weather');
-    window.location.href = 'https://suzygiv.github.io/Weather-Dashboard/';
+    window.open('https://suzygiv.github.io/Weather-Dashboard/', "_blank");
 });
 
 $('#weatherGitBtn').on('click', function () {
     console.log('Weather Github');
-    window.location.href = 'https://github.com/suzygiv/Weather-Dashboard';
+    window.open('https://github.com/suzygiv/Weather-Dashboard', "_blank");
 });
 
 //Code Quiz Buttons
 $('#codeBtn').on('click', function () {
     console.log('Code');
-    window.location.href = 'https://suzygiv.github.io/Code-Quiz/';
+    window.open('https://suzygiv.github.io/Code-Quiz/', "_blank");
 });
 
 $('#codeGitBtn').on('click', function () {
     console.log('Code Github');
-    window.location.href = 'https://github.com/suzygiv/Code-Quiz';
+    window.open('https://github.com/suzygiv/Code-Quiz', "_blank");
 });
 
 //Eat da Burger Buttons
 $('#burgerBtn').on('click', function () {
     console.log('Burger');
-    window.location.href = 'https://suzy-eat-da-burger.herokuapp.com/';
+    window.open('https://suzy-eat-da-burger.herokuapp.com/', "_blank");
 });
 
 $('#burgerGitBtn').on('click', function () {
     console.log('Burger Github');
-    window.location.href = 'https://github.com/suzygiv/Eat-Da-Burger';
+    window.open('https://github.com/suzygiv/Eat-Da-Burger', "_blank");
 });
 
 //Work Day Scheduler Buttons
 $('#workDayBtn').on('click', function () {
     console.log('Work Day');
-    window.location.href = 'https://suzygiv.github.io/Work-Day-Scheduler/';
+    window.open('https://suzygiv.github.io/Work-Day-Scheduler/', "_blank");
 });
 
 $('#workDayGitBtn').on('click', function () {
     console.log('Work Day Github');
-    window.location.href = 'https://github.com/suzygiv/Work-Day-Scheduler';
+    window.open('https://github.com/suzygiv/Work-Day-Scheduler', "_blank");
 });
 
 //Note Taker Buttons
 $('#noteTakerBtn').on('click', function () {
     console.log('Note Taker');
-    window.location.href = 'https://suzy-note-taker.herokuapp.com/';
+    window.open('https://suzy-note-taker.herokuapp.com/', "_blank");
 });
 
 $('#noteTakerGitBtn').on('click', function () {
     console.log('Note Taker Github');
-    window.location.href = 'https://github.com/suzygiv/Note-Taker';
+    window.open('https://github.com/suzygiv/Note-Taker', "_blank");
 });
 
 //WrappIt Buttons
 $('#wrappItGitBtn').on('click', function () {
     console.log('WrappIt Github');
-    window.location.href = 'https://github.com/suzygiv/WrappIt';
+    window.open('https://github.com/suzygiv/WrappIt', "_blank");
 });
 
 $('#wrappItBtn').on('click', function () {
     console.log('WrappIt');
-    window.location.href = 'https://intense-falls-86763.herokuapp.com/';
+    window.open('https://intense-falls-86763.herokuapp.com/', "_blank");
 });
 
 //Password Generator Buttons
 $('#passwordGeneratorGitBtn').on('click', function () {
     console.log('Password Generator Github');
-    window.location.href = 'https://github.com/suzygiv/Password-Generator';
+    window.open('https://github.com/suzygiv/Password-Generator', "_blank");
 });
 
 $('#passwordGeneratorBtn').on('click', function () {
     console.log('Password Generator');
-    window.location.href = 'https://suzygiv.github.io/Password-Generator/';
+    window.open('https://suzygiv.github.io/Password-Generator/', "_blank");
 });
 
 //Professional README Generator Button
 $('#professionalReadMeGitBtn').on('click', function () {
     console.log('Professional README Generator Github');
-    window.location.href = 'https://github.com/suzygiv/Professional-README-Generator';
+    window.open('https://github.com/suzygiv/Professional-README-Generator', "_blank");
 });
 
 $('#professionalReadMeVideoBtn').on('click', function () {
     console.log('Professional README Generator');
-    window.location.href = 'https://drive.google.com/file/d/1lWWqcFvo5wtpWsU32Mezlv9q5jURugwo/view';
+    window.open('https://drive.google.com/file/d/1lWWqcFvo5wtpWsU32Mezlv9q5jURugwo/view', "_blank");
 });
 
 //Employee Directory Buttons
 $('#employeeDGitBtn').on('click', function () {
     console.log('Employee Directory Github');
-    window.location.href = 'https://github.com/suzygiv/Employee-Directory';
+    window.open('https://github.com/suzygiv/Employee-Directory', "_blank");
 });
 
 $('#employeeDBtn').on('click', function () {
     console.log('Employee Directory');
-    window.location.href = 'https://suzygiv.github.io/Employee-Directory/';
+    window.open( 'https://suzygiv.github.io/Employee-Directory/', "_blank");
 });
 
 //Progressive Budget Buttons
 $('#progressiveBudgetGitBtn').on('click', function () {
     console.log('Progressive Budget Github');
-    window.location.href = 'https://github.com/suzygiv/Progressive-Budget';
+    window.open('https://github.com/suzygiv/Progressive-Budget', "_blank");
 });
 
 $('#progressiveBudgetBtn').on('click', function () {
     console.log('Progressive Budget');
-    window.location.href = 'https://powerful-anchorage-42548.herokuapp.com/';
+    window.open('https://powerful-anchorage-42548.herokuapp.com/', "_blank");
 });
 
 //Fitness Tracker Buttons
 $('#fitnessTrackerGitBtn').on('click', function () {
     console.log('Fitness Tracker Github');
-    window.location.href = 'https://github.com/suzygiv/Fitness-Tracker';
+    window.open('https://github.com/suzygiv/Fitness-Tracker', "_blank");
 });
 
 $('#fitnessTrackerBtn').on('click', function () {
     console.log('Fitness Tracker');
-    window.location.href = 'https://cryptic-brushlands-95540.herokuapp.com/?id=5ff87bd1ab7257001702c139';
+    window.open('https://cryptic-brushlands-95540.herokuapp.com/?id=5ff87bd1ab7257001702c139', "_blank");
 });
 
 //Team Profile Generator Buttons
 $('#teamProfileGitBtn').on('click', function () {
     console.log('Team Profile Generator Github');
-    window.location.href = 'https://github.com/suzygiv/Team-Profile-Generator';
+    window.open('https://github.com/suzygiv/Team-Profile-Generator', "_blank");
 });
 
 $('#teamProfileBtn').on('click', function () {
     console.log('Team Profile Generator');
-    window.location.href = 'https://drive.google.com/file/d/1-iYR_vyOpaHbox9mUFD9_pj3eetaFgi7/view';
+    window.open('https://drive.google.com/file/d/1-iYR_vyOpaHbox9mUFD9_pj3eetaFgi7/view', "_blank");
 });
 
 //Google Books React Search Buttons
 $('#googleGitBtn').on('click', function () {
     console.log('Google Books React Search Github');
-    window.location.href = 'https://github.com/suzygiv/Google-Books-React-Search';
+    window.open('https://github.com/suzygiv/Google-Books-React-Search', "_blank");
 });
 
 $('#googleBtn').on('click', function () {
     console.log('Google Books React Search');
-    window.location.href = 'https://pure-everglades-03286.herokuapp.com/';
+    window.open('https://pure-everglades-03286.herokuapp.com/', "_blank");
 });
 
 //React Portfolio Buttons
 $('#reactGitBtn').on('click', function () {
     console.log('React Portfolio Github');
-    window.location.href = 'https://github.com/suzygiv/React-Portfolio';
+    window.open('https://github.com/suzygiv/React-Portfolio', "_blank");
 });
 
 $('#reactBtn').on('click', function () {
     console.log('React Portfolio');
-    window.location.href = 'https://suzygiv.github.io/React-Portfolio/';
+    window.open('https://suzygiv.github.io/React-Portfolio/', "_blank");
 });
 
 //Code Refactor Buttons
 $('#codeRGitBtn').on('click', function () {
     console.log('Code Refactor Github');
-    window.location.href = 'https://github.com/suzygiv/Code-Refactor';
+    window.open('https://github.com/suzygiv/Code-Refactor', "_blank");
 });
 
 $('#codeRBtn').on('click', function () {
     console.log('Code Refactor');
-    window.location.href = 'https://suzygiv.github.io/Code-Refactor/';
+    window.open('https://suzygiv.github.io/Code-Refactor/', "_blank");
 });
 
 //Contact Submit Button
