03d2881999898a619fdcbecd47a13e7769727e52
suzygiv <suzannegivnish@gmail.com>
2021-02-04 16:02:30 -0500

    added to my portfolio page

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//Travel Companion Buttons
 $('#travelBtn').on('click', function () {
     console.log('Travel');
     window.location.href = 'https://dandandanoneil.github.io/travel-companion/index.html';
@@ -9,6 +9,7 @@ $('#travelGitBtn').on('click', function () {
     window.location.href = 'https://github.com/dandandanoneil/travel-companion';
 });
 
+//Weather Dashboard Buttons
 $('#weatherBtn').on('click', function () {
     console.log('Weather');
     window.location.href = 'https://suzygiv.github.io/Weather-Dashboard/';
@@ -19,6 +20,7 @@ $('#weatherGitBtn').on('click', function () {
     window.location.href = 'https://github.com/suzygiv/Weather-Dashboard';
 });
 
+//Code Quiz Buttons
 $('#codeBtn').on('click', function () {
     console.log('Code');
     window.location.href = 'https://suzygiv.github.io/Code-Quiz/';
@@ -29,6 +31,7 @@ $('#codeGitBtn').on('click', function () {
     window.location.href = 'https://github.com/suzygiv/Code-Quiz';
 });
 
+//Eat da Burger Buttons
 $('#burgerBtn').on('click', function () {
     console.log('Burger');
     window.location.href = 'https://suzy-eat-da-burger.herokuapp.com/';
@@ -39,6 +42,7 @@ $('#burgerGitBtn').on('click', function () {
     window.location.href = 'https://github.com/suzygiv/Eat-Da-Burger';
 });
 
+//Work Day Scheduler Buttons
 $('#workDayBtn').on('click', function () {
     console.log('Work Day');
     window.location.href = 'https://suzygiv.github.io/Work-Day-Scheduler/';
@@ -49,6 +53,7 @@ $('#workDayGitBtn').on('click', function () {
     window.location.href = 'https://github.com/suzygiv/Work-Day-Scheduler';
 });
 
+//Note Taker Buttons
 $('#noteTakerBtn').on('click', function () {
     console.log('Note Taker');
     window.location.href = 'https://suzy-note-taker.herokuapp.com/';
@@ -59,6 +64,7 @@ $('#noteTakerGitBtn').on('click', function () {
     window.location.href = 'https://github.com/suzygiv/Note-Taker';
 });
 
+//WrappIt Buttons
 $('#wrappItGitBtn').on('click', function () {
     console.log('WrappIt Github');
     window.location.href = 'https://github.com/suzygiv/WrappIt';
@@ -69,22 +75,62 @@ $('#wrappItBtn').on('click', function () {
     window.location.href = 'https://intense-falls-86763.herokuapp.com/';
 });
 
+//Password Generator Buttons
 $('#passwordGeneratorGitBtn').on('click', function () {
-    console.log('WrappIt Github');
+    console.log('Password Generator Github');
     window.location.href = 'https://github.com/suzygiv/Password-Generator';
 });
 
 $('#passwordGeneratorBtn').on('click', function () {
-    console.log('WrappIt');
+    console.log('Password Generator');
     window.location.href = 'https://suzygiv.github.io/Password-Generator/';
 });
 
+//Professional README Generator Button
 $('#professionalReadMeGitBtn').on('click', function () {
-    console.log('WrappIt');
+    console.log('Professional README Generator Github');
     window.location.href = 'https://github.com/suzygiv/Professional-README-Generator';
 });
 
+$('#professionalReadMeVideoBtn').on('click', function () {
+    console.log('Professional README Generator');
+    window.location.href = 'https://drive.google.com/file/d/1lWWqcFvo5wtpWsU32Mezlv9q5jURugwo/view';
+});
 
+//Employee Directory Buttons
+$('#employeeDGitBtn').on('click', function () {
+    console.log('Employee Directory Github');
+    window.location.href = 'https://github.com/suzygiv/Employee-Directory';
+});
+
+$('#employeeDBtn').on('click', function () {
+    console.log('Employee Directory');
+    window.location.href = 'https://suzygiv.github.io/Employee-Directory/';
+});
+
+//Progressive Budget Buttons
+$('#progressiveBudgetGitBtn').on('click', function () {
+    console.log('Progressive Budget Github');
+    window.location.href = 'https://github.com/suzygiv/Progressive-Budget';
+});
+
+$('#progressiveBudgetBtn').on('click', function () {
+    console.log('Progressive Budget');
+    window.location.href = 'https://powerful-anchorage-42548.herokuapp.com/';
+});
+
+//Fitness Tracker Buttons
+$('#fitnessTrackerGitBtn').on('click', function () {
+    console.log('Fitness Tracker Github');
+    window.location.href = 'https://github.com/suzygiv/Fitness-Tracker';
+});
+
+$('#fitnessTrackerBtn').on('click', function () {
+    console.log('Fitness Tracker');
+    window.location.href = 'https://cryptic-brushlands-95540.herokuapp.com/?id=5ff87bd1ab7257001702c139';
+});
+
+//Contact Submit Button
 $('#submitBtn').on('click', function () {
     console.log('Submit');
     window.location.href = 'mailto:suzannegivnish@gmail.com';
